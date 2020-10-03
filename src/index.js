9e3107f0af4a7c611c70b3001c9369eacc0c1d8f
Yuliya Kryvorot <krviua@yahoo.com>
2020-10-03 12:39:01 +0300

    feat: add solution to the task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 
 exports.min = function min (array) {
-  return 0;
+  return (arguments.length == 0 || array.length == 0 ? 0 : Math.min.apply(null, array));
 }
 
 exports.max = function max (array) {
-  return 0;
+  return (arguments.length == 0 || array.length == 0 ? 0 : Math.max.apply(null, array));
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if(arguments.length == 0 || array.length == 0) {
+    return 0;
+  }
+    let sum = 0;
+    array.forEach(i => sum += i);
+    return sum/array.length;
 }
